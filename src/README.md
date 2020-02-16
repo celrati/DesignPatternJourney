f200489a31cbfab402d7e1e0e029ea00c9dc860c
celrati <m.charif.achraf@gmail.com>
2020-02-16 17:25:48 +0100

    SPR introduction

---
+++ src/README.md
@@ -0,0 +1,32 @@
+# DesignPatternJourney
+\m/ \m/ \m/ Hard Rock journey of design pattern
+## Creational:
+* Builder
+* Factories
+    * Abstract Factory
+    * Factory Method
+* Prototype
+* Singleton
+
+## Structural:
+* Adapter
+* Bridge
+* Composite
+* Decorator
+* façade
+* Flyweight
+* Proxy
+
+## Behavioral:
+* Chain of responsibility
+* Command
+* Interpreter
+* Iterator
+* Mediator
+* Memento
+* Null Object
+* Observer
+* State
+* Strategy
+* Template Method
+* Visitor
---
+++ src/solidDesignPrinciples/SRP.java
@@ -0,0 +1,68 @@
+package solidDesignPrinciples;
+
+
+/*
+* Single Responsibilty Principle..
+* is to avoid making God Classes, instead make one class with one responsibility to avoid the anti-pattern..
+* example with Journal class..
+ */
+
+import java.io.File;
+import java.io.PrintStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SRP {
+    
+}
+
+class Journal
+{
+    private final List<String> entries = new ArrayList<>();
+
+    private static int count = 0;
+
+    public void addEntry(String text)
+    {
+        entries.add("" + (++count) + ": " + text);
+    }
+
+    public void removeEntry(int index)
+    {
+        entries.remove(index);
+    }
+
+    @Override
+    public String toString() {
+        return String.join(System.lineSeparator(), entries);
+    }
+
+    // here we break SRP
+    public void save(String filename) throws Exception
+    {
+        try (PrintStream out = new PrintStream(filename))
+        {
+            out.println(toString());
+        }
+    }
+
+    public void load(String filename) {}
+    public void load(URL url) {}
+}
+
+// handles the responsibility of persisting objects
+class Persistence
+{
+    public void saveToFile(Journal journal,
+                           String filename, boolean overwrite) throws Exception
+    {
+        if (overwrite || new File(filename).exists())
+            try (PrintStream out = new PrintStream(filename)) {
+                out.println(journal.toString());
+            }
+    }
+
+    public void load(Journal journal, String filename) {}
+    public void load(Journal journal, URL url) {}
+}
